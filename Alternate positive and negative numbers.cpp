e204f5b4b94d047f0a32ffe53f4c3c3c3c18ef0f
datapirates0101 <72863747+datapirates0101@users.noreply.github.com>
2022-08-27 15:25:43 +0530

    Update Alternate positive and negative numbers.cpp

--- Alternate positive and negative numbers.cpp
+++ Alternate positive and negative numbers.cpp
@@ -26,3 +26,46 @@ void rearrange(int arr[], int n) {
        }
    }
 };
+
+// using queue
+class Solution{
+public:
+
+	void rearrange(int arr[], int n) {
+	    queue<int> q1;
+	    queue<int> q2;
+	    
+	    for(int i=0; i<n; i++)
+	    {
+	        if(arr[i] < 0)
+	            q2.push(arr[i]);
+	        else
+	            q1.push(arr[i]);
+	    }
+	    
+	    int i=0;
+	    while(!q1.empty() && !q2.empty())
+	    {
+	        arr[i] = q1.front();
+	        q1.pop();
+	        i++;
+	        arr[i] = q2.front();
+	        q2.pop();
+	        i++;
+	    }
+	    
+	    while(!q1.empty())
+	    {
+	        arr[i] = q1.front();
+	        q1.pop();
+	        i++;
+	    }
+	    
+	     while(!q2.empty())
+	    {
+	        arr[i] = q2.front();
+	        q2.pop();
+	        i++;
+	    }
+	}
+};
